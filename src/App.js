877aa6a6edea413acec6efff054f74d4191ea578
Winston Smith <silverflash150@gmail.com>
2020-05-19 10:11:17 +0300

    slider new version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
 import "./App.css";
 
-import Header from "./component/Header/Header";
-import Main from "./component/Main/Main";
-import Footer from "./component/Footer/Footer";
-
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <img src="/arrow-icon/right-arrow.svg" alt="" />
     </div>
   );
 }
